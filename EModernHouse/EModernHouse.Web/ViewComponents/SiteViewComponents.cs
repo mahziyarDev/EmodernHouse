99f4b134f9bd57dc2d3ac0268296dbbd6634c209
mahziyar.Dev <mahziyar138006@gmail.com>
2021-07-16 15:49:16 +0430

    add Template

---
+++ EModernHouse/EModernHouse.Web/ViewComponents/SiteViewComponents.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EModernHouse.Web.ViewComponents
+{
+    #region SiteHeader
+
+    public class SiteHeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteHeader");
+        }
+    }
+
+    #endregion
+
+    #region SiteFooter
+
+    public class SiteFooterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteFooter");
+        }
+    }
+
+    #endregion
+}
